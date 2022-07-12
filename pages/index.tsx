aeb3f3a7718cbb6a4c2e5133bf19f87d21fcd4ff
rohanshrestha09 <shresthacr11@gmail.com>
2022-07-12 10:32:12 +0545

    nice

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,18 @@ import Contact from "../components/Contact";
 import Nav from "../components/Nav";
 import Works from "../components/Work";
 import Skills from "../components/Skills";
-import Script from "next/script";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <div className="w-full overflow-x-hidden xl:px-44 lg:px-20 px-7 bg-[rgb(40,42,57)] text-white font-sans">
       <Head>
         <title>Rohan Shrestha</title>
-        <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-        <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-        <script>AOS.init()</script>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
